1d982bb5e8c03976b00b3f3a7eaedd18ed8c4108
Gabriel Menacho <menacho.sjc@gmail.com>
2021-07-20 20:02:58 -0300

    back-end final

--- src/main/java/com/menacho/Projeto/Rest/ClienteController.java
+++ src/main/java/com/menacho/Projeto/Rest/ClienteController.java
@@ -1,5 +1,7 @@
 package com.menacho.Projeto.Rest;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -28,7 +31,7 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar(@RequestBody Cliente cliente) {
+	public Cliente salvar(@RequestBody @Valid Cliente cliente) {
 		return repository.save(cliente);
 	}	
 	
@@ -36,7 +39,7 @@ public class ClienteController {
 	public Cliente buscarPorId(@PathVariable Integer id) {
 		return repository
 				.findById(id)
-				.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
+				.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
 	}
 	
 	@DeleteMapping("{id}")
@@ -47,17 +50,17 @@ public class ClienteController {
 		.map( cliente -> {
 			repository.delete(cliente);
 			return Void.TYPE;
-		}).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		}).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
 	}
 	
 	@PutMapping("{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void atualizar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado) {
+	public void atualizar(@PathVariable Integer id, @RequestBody @Valid Cliente clienteAtualizado) {
 		repository
 		.findById(id)
 		.map( cliente-> {
 			clienteAtualizado.setId(cliente.getId());
 			return repository.save(clienteAtualizado);
-		}).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		}).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
 	}	
 }
